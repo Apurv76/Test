bed027bef9a1a0c6e31df16240c217b8523eca22
Apurv76 <104066015+Apurv76@users.noreply.github.com>
2022-04-21 14:21:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
-
+var box;
 function setup() {
   createCanvas(400,400);
+
+  var box = createSprite(200,200,30,30);
 }
 
 function draw() 
 {
-  background("blue");
+drawSprites();
+  if(keyDown("left")) {
+    background("red");
+  }
+
+  if(keyDown("right")) {
+    background("yellow");
+  }
 
 }
+ 
+
+
+
+
+
+
+
 
 
 
